6612859ab717072b6e3e65f807856a6f0f26b432
Juan Caicedo <juan@spanishdict.com>
2017-01-25 08:48:57 -0500

    responsive accent box

--- test/accent-box.js
+++ test/accent-box.js
@@ -5,128 +5,197 @@ describe('Accent box', function() {
 
   var frame;
 
-  before(function(done) {
-    frame = quixote.createFrame({
-      stylesheet: '/base/src/client/screen.css'
-    }, done);
-  });
-
   after(function() {
     frame.remove();
   });
 
-  describe('with one line of text', function() {
+  describe('desktop', function() {
 
-    var container;
-    var body;
-    var box;
-    var iconContainer;
-    var textBox;
-    var text;
+    before(function(done) {
+      frame = quixote.createFrame({
+        stylesheet: '/base/src/client/screen.css',
+        width: 1280,
+        height: 800
+      }, done);
+    });
 
-    beforeEach(function() {
-      frame.reset();
-      container = frame.add(
-        `<div class="sd-accent-box">
-          <div class="sd-accent-box-row">
-            <div class="sd-accent-box-icon-container">
-              <div class="sd-accent-box-icon">
+    describe('with one line of text', function() {
+
+      var container;
+      var body;
+      var box;
+      var iconContainer;
+      var textBox;
+      var text;
+
+      beforeEach(function() {
+        frame.reset();
+        frame.resize(1280, 800);
+        container = frame.add(
+          `<div class="sd-accent-box">
+            <div class="sd-accent-box-row">
+              <div class="sd-accent-box-icon-container">
+                <div class="sd-accent-box-icon">
+                </div>
+              </div>
+              <div class="sd-accent-box-text">
+                <h3>Did you know...</h3>
               </div>
             </div>
-            <div class="sd-accent-box-text">
-              <h3>Did you know...</h3>
+          </div>`
+        );
+        body = frame.get('body');
+        box = frame.get('.sd-accent-box');
+        iconContainer = frame.get('.sd-accent-box-icon-container');
+        textBox = frame.get('.sd-accent-box-text');
+        text = frame.get('h3');
+
+      });
+
+      it('takes up full width', function() {
+        box.assert({
+          width: body.width
+        });
+      });
+
+      it('aligns icon box and text box next to each other', function() {
+        iconContainer.assert({
+          top: textBox.top,
+          bottom: textBox.bottom
+        });
+      });
+
+      it('has an icon box with a set width', function() {
+        iconContainer.assert({
+          width: 70
+        });
+      });
+
+      it('has a text box that takes up remaining width', function() {
+        textBox.assert({
+          width: body.width.minus(70)
+        });
+      });
+
+      it('centers text within box with padding', function() {
+        text.assert({
+          top: textBox.top.plus(15),
+          right: textBox.right.minus(15),
+          bottom: textBox.bottom.minus(15),
+          left: textBox.left.plus(15)
+        });
+      });
+
+    });
+
+    describe('with multiple lines of text', function() {
+
+      var container;
+      var body;
+      var box;
+      var iconContainer;
+      var textBox;
+      var h1;
+      var h3;
+      var div;
+
+      beforeEach(function() {
+        frame.reset();
+        container = frame.add(
+          `<div class="sd-accent-box">
+            <div class="sd-accent-box-row">
+              <div class="sd-accent-box-icon-container">
+                <div class="sd-accent-box-icon">
+                </div>
+              </div>
+              <div class="sd-accent-box-text">
+                <h3>Did you know...</h3>
+                <div class="other">Other text</div>
+                <h1>Last header</h1>
+              </div>
             </div>
-          </div>
-        </div>`
-      );
-      body = frame.get('body');
-      box = frame.get('.sd-accent-box');
-      iconContainer = frame.get('.sd-accent-box-icon-container');
-      textBox = frame.get('.sd-accent-box-text');
-      text = frame.get('h3');
+          </div>`
+        );
+        body = frame.get('body');
+        box = frame.get('.sd-accent-box');
+        iconContainer = frame.get('.sd-accent-box-icon-container');
+        textBox = frame.get('.sd-accent-box-text');
+        h1 = frame.get('h1');
+        h3 = frame.get('h3');
+        div = frame.get('.other');
 
-    });
-
-    it('takes up full width', function() {
-      box.assert({
-        width: body.width
       });
-    });
 
-    it('aligns icon box and text box are next to each other', function() {
-      iconContainer.assert({
-        top: textBox.top,
-        bottom: textBox.bottom
+      it('maintains spacing after headers', function() {
+        div.assert({
+          top: h3.bottom.plus(18.720)
+        });
       });
-    });
 
-    it('has an icon box with a set width', function() {
-      iconContainer.assert({
-        width: 70
-      });
     });
-
-    it('has a text box that takes up remaining width', function() {
-      textBox.assert({
-        width: body.width.minus(70)
-      });
-    });
-
-    it('centers text within box with padding', function() {
-      text.assert({
-        top: textBox.top.plus(15),
-        right: textBox.right.minus(15),
-        bottom: textBox.bottom.minus(15),
-        left: textBox.left.plus(15)
-      });
-    });
-
   });
 
-  describe('with multiple lines of text', function() {
+  describe('mobile', function() {
 
-    var container;
-    var body;
-    var box;
-    var iconContainer;
-    var textBox;
-    var h1;
-    var h3;
-    var div;
+    before(function(done) {
+      frame = quixote.createFrame({
+        stylesheet: '/base/src/client/screen.css',
+        width: 800,
+        height: 736
+      }, done);
+    });
 
-    beforeEach(function() {
-      frame.reset();
-      container = frame.add(
-        `<div class="sd-accent-box">
-          <div class="sd-accent-box-row">
-            <div class="sd-accent-box-icon-container">
-              <div class="sd-accent-box-icon">
+    describe('with one line of text', function() {
+
+      var container;
+      var body;
+      var box;
+      var iconContainer;
+      var textBox;
+      var text;
+
+      beforeEach(function() {
+        frame.reset();
+        container = frame.add(
+          `<div class="sd-accent-box">
+            <div class="sd-accent-box-row">
+              <div class="sd-accent-box-icon-container">
+                <div class="sd-accent-box-icon">
+                </div>
+              </div>
+              <div class="sd-accent-box-text">
+                <h3>Did you know...</h3>
               </div>
             </div>
-            <div class="sd-accent-box-text">
-              <h3>Did you know...</h3>
-              <div class="other">Other text</div>
-              <h1>Last header</h1>
-            </div>
-          </div>
-        </div>`
-      );
-      body = frame.get('body');
-      box = frame.get('.sd-accent-box');
-      iconContainer = frame.get('.sd-accent-box-icon-container');
-      textBox = frame.get('.sd-accent-box-text');
-      h1 = frame.get('h1');
-      h3 = frame.get('h3');
-      div = frame.get('.other');
+          </div>`
+        );
+        body = frame.get('body');
+        box = frame.get('.sd-accent-box');
+        iconContainer = frame.get('.sd-accent-box-icon-container');
+        textBox = frame.get('.sd-accent-box-text');
+        text = frame.get('h3');
 
-    });
-
-    it('maintains spacing after headers', function() {
-      div.assert({
-        top: h3.bottom.plus(18.720)
       });
-    });
 
+      it('takes up full width', function() {
+        box.assert({
+          width: body.width
+        });
+      });
+
+      it('has an icon box with a set width', function() {
+        iconContainer.assert({
+          width: 50
+        });
+      });
+
+      it('has a text box that takes up remaining width', function() {
+        textBox.assert({
+          width: body.width.minus(50)
+        });
+      });
+
+    });
   });
 
 });
