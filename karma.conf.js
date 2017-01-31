1c8d54604c47e2022377c92754edb6c5a85c40fa
Juan Caicedo <juan@spanishdict.com>
2017-01-31 08:28:19 -0500

    ability to test safari

--- karma.conf.js
+++ karma.conf.js
@@ -80,8 +80,9 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    // browsers: ['ie_10'],
     browsers: ['Chrome'],
+    // browsers: ['ie_10'],
+    // browsers: ['Safari'],
 
 
     // Continuous Integration mode
